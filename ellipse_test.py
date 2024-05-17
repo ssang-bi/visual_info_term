080b15f2bc6ea113f9cf575569951a7f159a566f
ssang-bi <84119957+ssang-bi@users.noreply.github.com>
2024-05-17 14:32:00 +0900

    polygon_save_succes

--- ellipse_test.py
+++ ellipse_test.py
@@ -6,9 +6,16 @@ import math
 drawing = False
 ix, iy = (-1, -1)
 color = (0, 0, 0)
+count = 0
+
+def Ellipse_To_Image(image, roi_ellipse_center, roi_ellipse_axes, count) : # roi_ellipse 영역을 이미지에서 추출
+    mask = np.zeros_like(image)
+    cv2.ellipse(mask, roi_ellipse_center, roi_ellipse_axes, 0, 0, 360, (255, 255, 255), thickness=-1)
+    result = cv2.bitwise_and(image, mask)
+    cv2.imwrite(f"ksb{count:04d}.jpg", result)    
 
 def onMouse(event, x, y, flags, param):
-    global ix, iy, drawing
+    global ix, iy, drawing, count
     
     if event == cv2.EVENT_LBUTTONDOWN:
         drawing = True
@@ -21,17 +28,22 @@ def onMouse(event, x, y, flags, param):
             cv2.ellipse(tmpimg, center, axis, 0, 0, 360, color, 2)
             cv2.imshow("paint", tmpimg)
     elif event == cv2.EVENT_LBUTTONUP:
+        count += 1
         drawing = False
         center = ((ix + x) // 2, (iy + y) // 2)
         axis = (abs((ix - x) // 2), abs((iy - y) // 2))
         cv2.ellipse(param, center, axis, 0, 0, 360, color, 2)
-        cv2.imshow("paint", param)
-        
+        Ellipse_To_Image(param, center, axis, count)
+        cv2.imshow("paint", param)    
+
 def Brush():
-    img = np.full((480, 640), 255, np.uint8)
+    #img = np.full((480, 640), 255, np.uint8)
+    img = cv2.imread("input.jpg")
+    img = cv2.resize(img, (640, 480))
     cv2.namedWindow("paint")
     cv2.setMouseCallback("paint", onMouse, param=img)
     cv2.imshow("paint", img)
+    
     
     while True:
         
--- polygon_test.py
+++ polygon_test.py
@@ -3,41 +3,53 @@ import cv2
 import math
 
 drawing = False
-ix, iy = (-1, -1)
+ix, iy = -1, -1
 color = (0, 0, 0)
 count = 0
 pt = list()
+imgCount = 0
 
-def onMouse(event, x, y, flags, param):
-    global ix, iy, drawing, count, pt
+def polygonCapture(img, pt, imgCount):
+    mask = np.zeros_like(img)
+    pt_array = np.array(pt)
+    pt_array = pt_array.reshape((-1, 1, 2))
+    cv2.fillConvexPoly(mask, np.int32(pt_array), (255, 255, 255))
+    result = cv2.bitwise_and(img, mask)
+    cv2.imwrite(f"ksb{imgCount:04d}.jpg", result)
+
+def onMouse(event, x, y, flags, param): # 최적화 필요함 
+    global ix, iy, drawing, count, pt, imgCount
     
-    if event == cv2.EVENT_LBUTTONDOWN:
-        if drawing:
-            if count > 1:
-                if checkRad(pt[count - 2], pt[count - 1], (x, y)):              
-                    if calcLen(x, y, ix, iy) <= 20:
+    if event == cv2.EVENT_LBUTTONDOWN:  # 마우스 좌클릭 이벤트
+        if drawing: # polygon 그리기 시작
+            if count > 1:   # (3 + a)번쨰 점
+                if checkRad(pt[count - 2], pt[count - 1], (x, y)) and checkRad((x, y), (ix, iy), pt[1]):    # 좌표들 각도 유효한지 확인
+                    if calcLen(x, y, ix, iy) <= 20:     # 시작 좌표 랑 이전 좌표 연결
                         cv2.line(param, pt[count - 1], pt[0], color)
+                        imgCount += 1
+                        polygonCapture(param, pt, imgCount)
                         count = -1
                         pt.clear()
-                        drawing = False
-                    else: 
+                        drawing = False     # polygon 완성
+                    else:       # 좌표 찍기
                         pt.append((x, y))
                         cv2.circle(param, (x, y), 1, color)
                         cv2.line(param, pt[count - 1], pt[count], color)
-                else:
+                else:   # 유효하지 않은 좌표
                     count -= 1
             else:
                 pt.append((x, y))
                 cv2.circle(param, (x, y), 1, color)
                 cv2.line(param, pt[count - 1], pt[count], color)
-        else:
+        else:   # polygon 첫 좌표 찍기
             ix, iy = x, y
             pt.append((ix, iy))
             cv2.circle(param, (ix, iy), 1, color)
             drawing = True
         
         count += 1
-        print(count)
+        print(imgCount, count)    # 테스트 용
+        print(pt)
         cv2.imshow("paint", param)
     
 def calcLen(x1, y1, x2, y2):
@@ -62,7 +74,9 @@ def checkRad(pt1, pt2, pt3):   # 좌표를 pt1, pt2, pt3 순서대로 찍히면 
     
 
 def Brush():
-    img = np.full((480, 640), 255, np.uint8)
+    #img = np.full((480, 640), 255, np.uint8)
+    img = cv2.imread("input.jpg")
+    img = cv2.resize(img, (640, 480))
     cv2.namedWindow("paint")
     cv2.setMouseCallback("paint", onMouse, param=img)
     cv2.imshow("paint", img)
